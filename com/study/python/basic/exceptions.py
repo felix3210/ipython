ea5d0f07c0a560e4f2770a18a0778943218ae2b7
songfei <songfei@xbniao.com>
2017-04-01 11:31:56 +0800

    samples

--- com/study/python/basic/exceptions.py
+++ com/study/python/basic/exceptions.py
@@ -4,4 +4,76 @@
 # @Author  : felix
 # @Contact : wi1024u@gmail.com
 # @File    : exceptions.py
-# @Desc    : 
+# @Desc    : Introduce python exceptions & how to handle exception
+
+import sys
+
+# while True:
+#     try:
+#         x = int(raw_input("Please enter a number: "))
+#         break
+#     except ValueError, detail:
+#         print "Oops! That was no valid number . Try again ... %s " % detail
+#     except:
+#         print "Unexpected error:", sys.exc_info()
+# print '-'*100
+
+# try:
+#     f = open('/tmp/q.txt')
+#     s = f.readline()
+#     i = int(s.strip())
+# except IOError, (errno, strerror):
+#     print "I/O error(%s): %s" %(errno, strerror)
+# except ValueError:
+#     print "Could not convert data to an integer."
+# except:
+#     print "Unexpected error:", sys.exc_info()[0]
+#     raise
+# else:
+#     print 'file has', len(f.readlines()) , 'lines'
+#     f.close()
+# print '-'*100
+#
+#
+# try:
+#     raise Exception('spam', 'eggs')
+# except Exception, inst:
+#     print type(inst)
+#     print inst.args
+#     print inst
+#     x, y = inst
+#     print 'x =', x, '\n', 'y =', y
+# print '-'*100
+
+
+class MyError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+try:
+    raise MyError('custom except')
+except MyError, e:
+    print 'My exception occurred , value :', e.value
+print '-'*100
+
+with open('/tmp/q.txt') as f:
+    for line in f:
+        print line
+
+print '-'*100
+
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print 'division by zero!'
+    else:
+        print 'result is', result
+    finally:
+        print 'executing finally clause'
+divide(2, 1)
+divide(2, 0)
+divide("2", "1")
+print '-'*100
--- com/study/python/basic/file.py
+++ com/study/python/basic/file.py
@@ -5,3 +5,36 @@
 # @Contact : wi1024u@gmail.com
 # @File    : file.py
 # @Desc    : Reading and writing files
+
+import pickle
+f = open('/data/code/ipython/com/study/python/model/UserDTO.py')
+print f.read(10)
+print f.readline()
+print f.readlines()
+for line in f:
+    print line,
+print '-'*100
+f.seek(5)
+print f.read(1)
+f.seek(-3, 1)
+print f.read(1)
+f.close()
+print '-'*100
+
+f2 = open('/tmp/q.txt', 'r+')
+val = {'abc': 'hello', 'age': 100}
+f2.write(str(val))
+f2.close()
+print '-'*100
+
+f3 = open('/tmp/q.txt', 'r+')
+val = {'abc': 'hello', 'age': 100}
+pickle.dump(val, f3)
+f3.close()
+print '-'*100
+
+f4 = open('/tmp/q.txt')
+val = pickle.load(f4)
+print val
+f4.close()
+print '-'*100
